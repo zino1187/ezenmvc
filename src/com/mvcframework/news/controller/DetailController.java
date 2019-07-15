bcf16c77bb7bc671a8d5d854bab1529608173c20
zino1187 <zino1187@gmail.com>
2019-07-15 15:07:56 +0900

    no message

--- src/com/mvcframework/news/controller/DetailController.java
+++ src/com/mvcframework/news/controller/DetailController.java
@@ -7,25 +7,29 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.mvcframework.controller.Controller;
+import com.mvcframework.news.model.domain.News;
+import com.mvcframework.news.model.repository.NewsDAO;
 
 public class DetailController implements Controller{
-
+	NewsDAO newsDAO = new NewsDAO();
+	
 	@Override
 	public void doRequest(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		String news_id = request.getParameter("news_id");
+		News news= newsDAO.select(Integer.parseInt(news_id));//3단계
 		
+		request.setAttribute("news", news);//4단계: 가져갈것이 잇음..
 	}
 
 	@Override
 	public String getResultKey() {
-		// TODO Auto-generated method stub
-		return null;
+		return "/views/news/detail";
 	}
 
 	@Override
 	public boolean isForward() {
-		// TODO Auto-generated method stub
-		return false;
+		return true;	
 	}
 	
 }
